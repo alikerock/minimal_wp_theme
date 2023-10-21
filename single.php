c3c7f1334bc5452ffba6de4311586378b23b49d4
rock's ezweb <alikerock@gmail.com>
2023-10-21 12:21:21 +0900

    header, footer for single

---
+++ single.php
@@ -0,0 +1,80 @@
+<?php get_header('single'); ?>
+
+</header>
+   
+    <main class="content portoflio-single">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 decription">
+                    <div class="contents shadow">
+                        <img src="images/portfolio_single_img1.jpg" alt="img1">
+                        <p>image description 1</p>
+                    </div>
+                    <div class="contents shadow">
+                        <img src="images/portfolio_single_img2.jpg" alt="img2">
+                        <p>image description 2</p>
+                    </div>
+                </div>
+                <div class="col-md-4 portfolio_info">
+                    <div class="contents shadow">
+                        <h2>New Landing Page</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+                        <p class="link">
+                            <a href="">Visit site &rarr;</a>
+                        </p>
+                        <hr class="double">
+                        <blockquote>
+                            <p>“Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.” </p>
+                            <small>- JOHN DOE, ACME INC. -</small>
+                        </blockquote>
+                        <p class="nav">
+                            <a href="" class="secondary-btn">&larr; Previous Project</a>
+                            <a href="" class="secondary-btn">Next Project &rarr;</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>          
+    </main>
+    <div class="related_portfolio">
+        <div class="container latest_portfolio">
+            <h3 class="heading6">Related portfolio entries</h3>
+            <div class="row list">
+                <div class="col-md-4">
+                    <div class="contents shadow">
+                        <img src="images/latest_portfolio_01.jpg" alt="latest_portfolio_01">
+                        <div class="hover_contents">
+                            <div class="list_info">
+                                <h3><a href="">Project Title</a> <img src="images/portfolio_list_arrow.png" alt="list arrow"></h3>
+                                <p><a href="">Click to see project</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contents shadow">
+                        <img src="images/latest_portfolio_02.jpg" alt="latest_portfolio_02">
+                        <div class="hover_contents">
+                            <div class="list_info">
+                                <h3><a href="">Project Title</a> <img src="images/portfolio_list_arrow.png" alt="list arrow"></h3>
+                                <p><a href="">Click to see project</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="contents shadow">
+                        <img src="images/latest_portfolio_03.jpg" alt="latest_portfolio_03">
+                        <div class="hover_contents">
+                            <div class="list_info">
+                                <h3><a href="">Project Title</a> <img src="images/portfolio_list_arrow.png" alt="list arrow"></h3>
+                                <p><a href="">Click to see project</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php get_footer('single');?>
